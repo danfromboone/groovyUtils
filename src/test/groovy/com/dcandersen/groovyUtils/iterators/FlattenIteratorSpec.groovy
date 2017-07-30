812570d0976136dfcf3d3d224b873b06440b3752
Dan Andersen <dan@dcandersen.com>
2017-07-29 22:49:15 -0500

    test FlattenIterator

---
+++ src/test/groovy/com/dcandersen/groovyUtils/iterators/FlattenIteratorSpec.groovy
@@ -0,0 +1,25 @@
+package com.dcandersen.groovyUtils.iterators
+
+import groovy.transform.ToString
+import spock.lang.Specification
+
+@ToString
+class FlattenIteratorSpec extends Specification {
+
+    def "it works"(){
+        setup:
+
+        Iterator innterIterator = [[1,2,3,4],[5,6,7,8],[9,10,11]].iterator()
+
+        FlattenIterator flattenIterator = new FlattenIterator(innterIterator)
+
+        when:
+        def collectedItems = flattenIterator.collect()
+
+        then:
+
+        collectedItems == [1,2,3,4,5,6,7,8,9,10,11]
+
+    }
+
+}
